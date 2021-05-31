5ebb6663f0ff1505644c2216fb36274c12230f1e
AndyE <aenders@uwyo.edu>
2021-05-31 08:50:15 -0600

    Site request works

--- PythonProjects/Indeed_Scraper/Scraper.py
+++ PythonProjects/Indeed_Scraper/Scraper.py
@@ -4,3 +4,16 @@
 # it won't work
 
 # DO IT WITHOUT THE VIDEO!!!
+
+import requests
+from bs4 import BeautifulSoup
+
+# create a function that gets the website page
+
+def getSite():
+    website = 'https://www.indeed.com/jobs?q=software+developer&l=Denver%2C+CO'
+    req = requests.get(website)
+    return req.status_code
+
+
+print(getSite())
