47d33eb68abca347498cb4418c30ecde0965b30e
AndyE <aenders@uwyo.edu>
2021-06-11 13:20:37 -0600

    Will now obtain job locations

--- PythonProjects/Indeed_Scraper/Scraper.py
+++ PythonProjects/Indeed_Scraper/Scraper.py
@@ -1,26 +1,52 @@
 # Web scraper to scrape jobs off of Indeed
-# note, requests are limited on Indeed's side
-# don't request too many times in a short period or else
-# it won't work
 
-# DO IT WITHOUT THE VIDEO!!!
+
+# The program sometimes works
+# It seems almost as if the full html isn't being downloaded or
+# it is based on javascript and sometimes loads in and other times doesn't.
 
 import requests
 from bs4 import BeautifulSoup
 
-# create a function that gets the website page
+template = 'https://www.indeed.com/jobs?q=software+developer&l=Denver%2C+CO&fromage=14'
 
-def getSite():
-    website = 'https://www.indeed.com/jobs?q=software+developer&l=Denver%2C+CO'
-    html = requests.get(website).text
-    return html
+# create a function that gets the website page
+def getSite(position, location):
+    website = 'https://www.indeed.com/jobs?q=software+developer&l=Denver%2C+CO&fromage=14'
+    html = requests.get(website)
+    # print(html.status_code)
+    return html.text
 
 
 # create a function to parse the site
 def create_soup(html):
     soup = BeautifulSoup(html, 'html.parser')
-    print(soup.prettify())
+    return soup
 
 
-html = getSite()
-create_soup(html)
+# create a function to get the job-cards on indeed
+def get_job_cards(soup):
+    job_cards = soup.find_all('div','jobsearch-SerpJobCard')
+    if len(job_cards) == 0:
+        print("find_all cant find anything, try again")
+        return []
+    for job in job_cards:
+        name = job.find('a').text.strip()
+        company = job.find('span', 'company').text.strip()
+        location = job.find('div', 'recJobLoc').get('data-rc-loc')
+        print(name)
+        print(company)
+        print(location)
+        print('\n')
+
+    return job_cards
+
+
+def main():
+    html = getSite('software developer', 'denver co')
+    soup = create_soup(html)
+    jobs = get_job_cards(soup)
+    print(len(jobs))
+
+
+main()
